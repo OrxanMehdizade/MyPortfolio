bb3c3da1ebac59c00267ad35a212d5547d152595
Mehdi_ha01 <Mehdi_ha01@STHQ011A-11>
2024-04-21 07:44:47 +0400

    updated

---
+++ MyPortfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactMessage message)
+        {
+            if (ModelState.IsValid)
+            {
+                // Azure Function App'e HTTP isteği gönderme
+                // Mail gönderme işlemi burada yapılacak
+                // Örnek kod:
+                // HttpClient client = new HttpClient();
+                // HttpResponseMessage response = await client.PostAsJsonAsync("Azure Function URL", message);
+                // response.EnsureSuccessStatusCode(); // Başarılı bir yanıt alınmadığında hata yönetimi
+                ViewBag.SuccessMessage = "Your message has been sent successfully!";
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ MyPortfolio/Models/ContactMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace MyPortfolio.Models;
+
+public class ContactMessage
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Message { get; set; }
+}
